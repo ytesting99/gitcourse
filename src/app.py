eb86058fcd2fc9a16334a189b24ccece422debb9
ytesting99 <yasirtesting99@gmail.com>
2024-11-23 10:58:35 +0300

    feat(scr/app.py): added the update item route to API

--- src/app.py
+++ src/app.py
@@ -12,3 +12,28 @@ def create_item(item:Item):
     conn.commit()
     item.id = cursor.lastrowid
     return item
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int, item:Item):
+    """API route to update an item from database trought the item id.
+
+    Parameters
+    ----------
+    item_id : int
+        Item unique identification
+    item : Item
+        The item itself following the Item model
+
+    Returns
+    -------
+    Item
+        The item after it has been added to database
+    """
+    conn = get_db()
+    conn.execute(
+        "UPDATE items SET name = ?, price = ?, is_offer = ? WHERE id = ?",
+        (item.name, item.price, int(item.is_offer) 
+        if item.is_offer 
+        else None, item_id),)
+    conn.commit()
+    return item
